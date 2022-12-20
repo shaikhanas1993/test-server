67e22c96e0c6069140a48f01a13516f28c66b21b
Anas shaikh <shaikhanas1993@gmail.com>
2022-12-20 18:02:24 -0500

    added minor changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('test server example')
@@ -15,6 +17,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
